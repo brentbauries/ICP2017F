96c22d51e6d6d20674fb360a0a868cadbcad3cb7
brentbauries <31543156+brentbauries@users.noreply.github.com>
2017-10-02 09:03:18 -0500

    Add files via upload

---
+++ Homework/2/script_full_of_errors_fixed.m
@@ -0,0 +1,27 @@
+a2 = 2; %can't start with number
+b = 1;
+a1 = b;
+x = 2;
+y = x + 4; % is it 6?
+pi = 4 * atan(1); %semicolon which was in the middle
+disp(pi)
+pi = 3.14159; %two different quotes
+disp(tan(pi));
+c = 4^3^2^3;
+d = ((c-78564)/(c + 32)); %an additional parantheses and absent variable name
+year = 2017;
+disp(['The year is ' num2str(year)]); % does it display 2017 correctly?
+stuff = {'a' 'b' 4 21 'c'};
+beginning = stuff(1);
+endVar = stuff(5); %needed to add the Var
+discount = 12;% order with semicolon
+AMOUNT = 120.00; %-to finish
+amount = 120; %cannot use the $ in that manner
+%and = duck; no value so assumed a comment
+class = 'INF1100, gr 2'; %used '
+continue_ = x > 0;
+fox = false; %cannot use the double equal
+wolf = fox == true;
+Persian = ['Persian'' is ' 'a'' human ' 'language'];
+Spanish = {'Spanish ' 'is ' ' another'  'language'};
+disp([Persian(1:7) 'is not the same as' Spanish(1)]);
